1749332b5b938220ebe2603e4ba213fbc1b25be2
Artem-Borzov <abug990@gmail.com>
2023-03-07 01:30:11 +0200

    v

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,30 +4,31 @@ import Searchbar from 'components/Searchbar/Searchbar';
 import { fetchImages } from './../FetchAPI/FetchAPI';
 
 export default class App extends Component {
-  state = {
-    gallery: [],
-    searchString: '',
-    page: 1,
-    totalPage: 0,
-    modalItem: null,
-    isHaveLoadMore: false,
-    isLoading: false,
-  };
-  handleFormSubmit = searchQuery => {
-    this.setState({
-      searchQuery: searchQuery,
-      images: [],
-      page: 1,
-      showErrorMessage: false,
-    });
-  };
-  render() {
-    const { images, showModal, showLoader, showErrorMessage } = this.state;
+	state = {
+		searchQuery: '',
+		images: [],
+		page: 1,
+		error: null,
+		showLoader: false,
+		showModal: false,
+		modalImageURL: null,
+		showErrorMessage: false,
+	};
+	handleFormSubmit = searchQuery => {
+		this.setState({
+			searchQuery: searchQuery,
+			images: [],
+			page: 1,
+			showErrorMessage: false,
+		});
+	};
+	render() {
+		const { images, showModal, showLoader, showErrorMessage } = this.state;
 
-    return (
-      <>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-      </>
-    );
-  }
+		return (
+			<>
+				<Searchbar onSubmit={this.handleFormSubmit} />
+			</>
+		);
+	}
 }
